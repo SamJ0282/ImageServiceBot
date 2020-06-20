d29ea729dc7eb85f32fffeb78a10f07ef59d169f
Your Name <sam7075jain@gmail.com>
2020-06-20 17:49:48 +0530

    Generic message commit

--- app.py
+++ app.py
@@ -2,16 +2,154 @@ import os, sys
 from flask import Flask, request
 from pymessenger import Bot
 
+
+
+PAGE_ACCESS_TOKEN = os.environ["PAGE_ACCESS_TOKEN"]
+VERIFY_TOKEN = os.environ["VERIFY_TOKEN"]
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def verify():
 	# Webhook verification
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == "hello":
+        if not request.args.get("hub.verify_token") == VERIFY_TOKEN:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
     return "Hello world", 200
+
+
+@app.route('/', methods=['POST'])   
+def webhook():
+
+    # endpoint for processing incoming messaging events
+
+    data = request.get_json()
+    log(data)  # you may not want to log every incoming message in production, but it's good for testing
+
+    if data["object"] == "page":   # make sure this is a page subscription
+
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+
+                if messaging_event.get("message"):     # someone sent us a message
+                    received_message(messaging_event)
+
+                elif messaging_event.get("delivery"):  # delivery confirmation
+                    pass
+                    # received_delivery_confirmation(messaging_event)
+
+                elif messaging_event.get("optin"):     # optin confirmation
+                    pass
+                    # received_authentication(messaging_event)
+
+                elif messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                    received_postback(messaging_event)
+
+                else:    # uknown messaging_event
+                    log("Webhook received unknown messaging_event: " + messaging_event)
+
+    return "ok", 200
+
+def received_message(event):
+
+    sender_id = event["sender"]["id"]        # the facebook ID of the person sending you the message
+    recipient_id = event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+    
+    # could receive text or attachment but not both
+    if "text" in event["message"]:
+        message_text = event["message"]["text"]
+
+        # parse message_text and give appropriate response   
+        if message_text == 'image' or message_text == 'Image':
+            send_image_message(sender_id)
+
+        elif message_text == 'file' or message_text == 'File':
+            send_file_message(sender_id)
+
+        elif message_text == 'audio' or message_text == 'Audio':
+            send_audio_message(sender_id)
+
+        elif message_text == 'video' or message_text == 'Video':
+            send_video_message(sender_id)
+
+        elif message_text == 'button' or message_text == 'Button':
+            send_button_message(sender_id)
+
+        elif message_text == 'generic' or message_text == 'Generic':
+            send_generic_message(sender_id)
+
+        elif message_text == 'share' or message_text == 'Share':
+            send_share_message(sender_id)
+
+        else: # default case
+            send_text_message(sender_id, "Echo: " + message_text)
+
+    elif "attachments" in event["message"]:
+        message_attachments = event["message"]["attachments"]
+        send_text_message(sender_id, "Message with attachment received")
+        
+def send_generic_message(recipient_id):
+
+    message_data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Asylex",
+                        "subtitle": "free online legal aid on Swiss asylum law",
+                        "item_url": "https://asylex.ch/",               
+                        "image_url": "https://www.google.ch/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+                        "buttons": [{
+                            "type": "web_url",
+                            "url": "https://asylex.ch/docs/faq_en.pdf",
+                            "title": "Open FAQ"
+                        }, {
+                            "type": "postback",
+                            "title": "Call Postback",
+                            "payload": "Payload for first bubble",
+                        }],
+                    }, {
+                        "title": "Google",
+                        "subtitle": "Find all your answers",
+                        "item_url": "https://www.google.com/",               
+                        "image_url": "https://www.google.ch/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+                        "buttons": [{
+                            "type": "web_url",
+                            "url": "https://www.google.ch/",
+                            "title": "Google Suche"
+                        }, {
+                            "type": "postback",
+                            "title": "Call Postback",
+                            "payload": "Payload for second bubble",
+                        }]
+                    }]
+                }
+            }
+        }
+    })
+
+    log("sending template with choices to {recipient}: ".format(recipient=recipient_id))
+
+    call_send_api(message_data)
+
+def call_send_api(message_data):
+
+    params = {
+        "access_token": PAGE_ACCESS_TOKEN
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    
+    r = requests.post(https://graph.facebook.com/v7.0/me/messages, params=params, headers=headers, data=message_data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
 
 
 if __name__ == "__main__":
