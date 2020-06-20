57f418db4d2ef0cc8625fac3eedb61d46a4df086
Your Name <sam7075jain@gmail.com>
2020-06-20 18:44:12 +0530

    modified url

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ def call_send_api(message_data):
         "Content-Type": "application/json"
     }
     
-    r = requests.post("https://graph.facebook.com/v7.0/me/messages, params=params, headers=headers, data=message_data")
+    r = requests.post("https://graph.facebook.com/v7.0/me/messages", params=params, headers=headers, data=message_data)
     
 
 
